3677b44ed3219c46626be04419ba3f704be08dd0
Daniel Eliasson <daniel.eliasson@idnet.se>
2024-09-24 21:13:58 +0200

    Enhance Swagger and Ocelot configuration
    Updated Program.cs to customize Swagger setup with title "StileStream WMS API" and version "v1". Adjusted UseSwaggerUI() to set the endpoint and RoutePrefix to root URL. Moved UseOcelot() middleware call for proper execution order. Modified ocelot.json to include GlobalConfiguration with BaseUrl "http://localhost:5000".

--- StileStreamWms/src/ApiGateway/StileStream.Api.Gateway/Program.cs
+++ StileStreamWms/src/ApiGateway/StileStream.Api.Gateway/Program.cs
@@ -11,7 +11,10 @@ builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "StileStream WMS API", Version = "v1" });
+});
 builder.Services.AddOcelot();
 builder.Services.AddLogging(logging =>
 {
@@ -22,19 +25,28 @@ builder.Services.AddLogging(logging =>
 
 var app = builder.Build();
 
-await app.UseOcelot();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "StileStream WMS API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
+
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.UseOcelot();
 
 await app.RunAsync();
