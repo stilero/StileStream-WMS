05409e70434a1e301fd88ff862a9d15c85d9f7ad
Daniel Eliasson <daniel.eliasson@idnet.se>
2024-10-10 21:22:18 +0200

    Remove InventoryService and related infrastructure
    This commit removes the entire InventoryService and its related infrastructure. The following key changes were made:
    - Removed application service extensions, command handlers, validators, error definitions, request and response classes, domain events, repository interfaces, and project files.
    - Deleted Docker configurations, Azure Functions, and service configurations.
    - Removed Entity Framework Core setup, including DbContext, design-time factory, and multiple migration files.
    - Deleted entity configurations, entity definitions, repository implementations, and unit of work implementations.
    - Removed project files defining dependencies and project structure.
    
    These changes indicate a major refactor or decommissioning of the InventoryService and its associated components.

--- StileStreamWms/src/Inventory/StileStream.Wms.Inventory.Application/Products/ProductImport/Requests/ImportBulkProductRequest.cs
+++
@@ -1,2 +0,0 @@
-namespace StileStream.Wms.Inventory.Application.Products.ProductImport.Requests;
-public sealed record ImportBulkProductRequest(IReadOnlyCollection<ImportProductRequest> Products);
--- StileStreamWms/src/Inventory/StileStream.Wms.Inventory.Domain/Products/Events/ProductUpdatedEvent.cs
+++
@@ -1,6 +0,0 @@
-using StileStream.Wms.SharedKernel.Domain.Interfaces;
-using StileStream.Wms.Inventory.Domain.Products.Entities;
-
-namespace StileStream.Wms.Inventory.Domain.Products.Events;
-
-//public record ProductUpdatedEvent(Product Product) : IDomainEvent;
